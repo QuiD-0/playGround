4775fee5abe9f91f01630a4b7cb02795c8140fbd
QuiD-0 <wodnd101@naver.com>
2022-11-02 14:23:06 +0900

    init

---
+++ src/test/java/com/quid/Rx/RxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quid.Rx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
