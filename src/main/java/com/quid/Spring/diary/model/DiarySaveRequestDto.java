0f203a296eddbacece6023e04b820927cdfcb7c5
QuiD-0 <wodnd101@naver.com>
2022-11-17 17:55:12 +0900

    dairy : iso 적용

--- src/main/java/com/quid/Spring/diary/model/DiarySaveRequestDto.java
+++ src/main/java/com/quid/Spring/diary/model/DiarySaveRequestDto.java
@@ -3,9 +3,10 @@ package com.quid.Spring.diary.model;
 import com.quid.Spring.diary.Diary;
 import java.time.LocalDate;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.DateTimeFormat.ISO;
 
 public record DiarySaveRequestDto(String title, String content,
-                                  @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate date) {
+                                  @DateTimeFormat(iso = ISO.DATE) LocalDate date) {
 
     public Diary toEntity() {
         return Diary.builder()
