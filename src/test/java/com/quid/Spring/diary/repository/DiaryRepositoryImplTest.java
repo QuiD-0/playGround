3c49664ba8f704f52d9b62899be6ee535b2c5e02
QuiD-0 <wodnd101@naver.com>
2022-11-18 10:14:54 +0900

    dairy : bulk insert

--- src/test/java/com/quid/Spring/diary/repository/DiaryRepositoryImplTest.java
+++ src/test/java/com/quid/Spring/diary/repository/DiaryRepositoryImplTest.java
@@ -3,6 +3,7 @@ package com.quid.Spring.diary.repository;
 import com.quid.Spring.diary.Diary;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.UUID;
 import java.util.stream.IntStream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -16,17 +17,25 @@ class DiaryRepositoryImplTest {
     @Autowired
     DiaryRepository diaryRepository;
 
+
     @Test
     @Rollback(value = false)
     @DisplayName("테스트를 위한 데이터 삽입")
     public void bulkInsert() {
-        List<Diary> diaries = IntStream.range(0, 1000).mapToObj(i -> Diary.builder()
-            .date(LocalDate.now().minusDays((long) (Math.random() * 100)))
-            .title("title" + i)
-            .content("content" + i)
-            .build()).toList();
+        List<Diary> diaries = IntStream.range(0, 100)
+            .mapToObj(DiaryRepositoryImplTest::getRandomDiary)
+            .toList();
 
         diaryRepository.saveAll(diaries);
     }
 
+
+    public static Diary getRandomDiary(int i) {
+        UUID uuid = UUID.randomUUID();
+        return Diary.builder()
+            .date(LocalDate.now().minusDays((long) (Math.random() * 100)))
+            .title("title : " + uuid)
+            .content("content : " + uuid)
+            .build();
+    }
 }
