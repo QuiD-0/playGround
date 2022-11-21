224ab7ff292e4f312545574ff35c38fa6c0e63d6
quid-0 <wodnd101@naver.com>
2022-11-21 23:16:21 +0900

    FanOutTimeline : timeline 쿼리 작성

--- src/main/java/com/quid/Spring/FanOutTimeline/timeline/model/TimelineDto.java
+++ src/main/java/com/quid/Spring/FanOutTimeline/timeline/model/TimelineDto.java
@@ -1,5 +1,8 @@
 package com.quid.Spring.FanOutTimeline.timeline.model;
 
+import lombok.Builder;
+
+@Builder
 public record TimelineDto(String memberName, String title, String content, Long articleId) {
 
 }
--- src/main/java/com/quid/Spring/FanOutTimeline/timeline/repository/TimelineDslRepository.java
+++ src/main/java/com/quid/Spring/FanOutTimeline/timeline/repository/TimelineDslRepository.java
@@ -1,5 +1,10 @@
 package com.quid.Spring.FanOutTimeline.timeline.repository;
 
+import static com.quid.Spring.FanOutTimeline.article.QArticle.article;
+import static com.quid.Spring.FanOutTimeline.member.QMember.member;
+import static com.quid.Spring.FanOutTimeline.timeline.QTimeline.timeline;
+
+import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.quid.Spring.FanOutTimeline.timeline.model.TimelineDto;
 import java.util.List;
@@ -13,6 +18,13 @@ public class TimelineDslRepository {
     private final JPAQueryFactory jpaQueryFactory;
 
     public List<TimelineDto> getTimeline(Long memberId) {
-        return null;
+        return jpaQueryFactory.select(
+                Projections.constructor(TimelineDto.class, member.name, article.title, article.content,
+                    article.id))
+            .from(timeline)
+            .join(article).on(timeline.articleId.eq(article.id))
+            .join(member).on(timeline.memberId.eq(member.id))
+            .where(timeline.memberId.eq(memberId))
+            .fetch();
     }
 }
