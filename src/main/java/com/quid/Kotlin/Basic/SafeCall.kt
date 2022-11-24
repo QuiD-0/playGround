415a765557b8213be28a0f6f74d02534a883e44f
QuiD-0 <wodnd101@naver.com>
2022-11-24 09:43:35 +0900

    Kotlin : null safety

---
+++ src/main/java/com/quid/Kotlin/Basic/SafeCall.kt
@@ -0,0 +1,27 @@
+package com.quid.Kotlin.Basic
+
+import org.junit.jupiter.api.Test
+
+class SafeCall {
+
+    @Test
+    fun test() {
+        val a: String? = "abc"
+        val length = a?.length
+        println("length = $length")
+        val i = a?.length ?: -1
+        println("i = $i")
+        a!!.length
+
+        val b: String? = null
+
+        a?.run { println("a is not null") }
+        b ?: run { println("b is null") }
+
+        val c = a?.length ?: b?.length ?: -1
+        println("c = $c")
+
+        a?.startsWith("a") ?: throw IllegalArgumentException("a is null")
+    }
+
+}
