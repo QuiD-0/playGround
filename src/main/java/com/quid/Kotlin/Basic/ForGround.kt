f1a882fedeca81b7b02ecd946fc5c33635709c44
QuiD-0 <wodnd101@naver.com>
2022-11-24 17:23:39 +0900

    Kotlin : for

---
+++ src/main/java/com/quid/Kotlin/Basic/ForGround.kt
@@ -0,0 +1,21 @@
+package com.quid.Kotlin.Basic;
+
+import org.junit.jupiter.api.Test;
+
+public class ForGround {
+
+    @Test
+    fun test() {
+        val nums = listOf(1, 2, 3)
+        for (num in nums) {
+            println(num)
+        }
+
+        for (i in 1..3) {
+            println(i)
+        }
+
+        IntRange(1, 10).step(2).forEach { println(it) }
+    }
+
+}
