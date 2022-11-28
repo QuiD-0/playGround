11bbb6e62f12a9d5db8933ac68a0ef13f712a8d6
quid-0 <wodnd101@naver.com>
2022-11-28 22:44:10 +0900

    Kotlin : class

---
+++ src/main/java/com/quid/Kotlin/Basic/classGround/Person.kt
@@ -0,0 +1,19 @@
+package com.quid.Kotlin.Basic.classGround
+
+class Person private constructor(val name: String, var age: Int) {
+    init {
+        if (age < 0) throw IllegalArgumentException("Age must be greater than 0")
+    }
+
+    companion object {
+        fun of(): Person {
+            return Person("QuiD", 0)
+        }
+        fun of(name: String, age: Int): Person {
+            return Person(name, age)
+        }
+    }
+
+    val isAdult: Boolean
+        get() = this.age >= 18
+}
