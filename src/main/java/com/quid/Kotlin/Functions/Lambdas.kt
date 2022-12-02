c1842cc13e31eafb394acb153e15f3eb09f75a35
QuiD-0 <wodnd101@naver.com>
2022-12-02 16:53:04 +0900

    Kotlin : lambda

---
+++ src/main/java/com/quid/Kotlin/Functions/Lambdas.kt
@@ -0,0 +1,14 @@
+package com.quid.Kotlin.Functions
+
+fun main() {
+    val fruits = listOf("banana", "avocado", "apple", "kiwifruit")
+    isApple(fruits[0]).also { println(it) }
+
+    filterFruits(fruits) { it == "apple" }
+}
+
+fun isApple(name: String): Boolean = name == "apple"
+
+private fun filterFruits(fruits: List<String>, filter: (String) -> Boolean): List<String> {
+    return fruits.filter { filter(it) }
+}
