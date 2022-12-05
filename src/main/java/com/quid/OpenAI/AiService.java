60c768d9b3cf06507ed078d22951c56c0bd3bcc8
QuiD-0 <wodnd101@naver.com>
2022-12-05 16:54:45 +0900

    Ai : refactor code

---
+++ src/main/java/com/quid/OpenAI/AiService.java
@@ -0,0 +1,37 @@
+package com.quid.OpenAI;
+
+import com.theokanning.openai.OpenAiService;
+import com.theokanning.openai.completion.CompletionRequest;
+import com.theokanning.openai.completion.CompletionResult;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+public interface AiService {
+
+    CompletionResult getAiTalk(String text);
+
+    @Service
+    class AiServiceImpl implements AiService {
+
+        @Value("${openAi.apiKey}")
+        private static String key;
+
+        @Override
+        public CompletionResult getAiTalk(String text) {
+            OpenAiService service = new OpenAiService(key);
+            return service.createCompletion(getBuild(text));
+        }
+
+        private static CompletionRequest getBuild(String text) {
+            return CompletionRequest.builder()
+                .model("text-davinci-003")
+                .temperature(0.7)
+                .prompt(text)
+                .maxTokens(256)
+                .topP(1.0)
+                .frequencyPenalty(0.0)
+                .presencePenalty(0.0)
+                .build();
+        }
+    }
+}
--- src/main/java/com/quid/OpenAI/OpenAiGround.java
+++ src/main/java/com/quid/OpenAI/OpenAiGround.java
@@ -1,32 +1,20 @@
 package com.quid.OpenAI;
 
-import com.theokanning.openai.OpenAiService;
-import com.theokanning.openai.completion.CompletionRequest;
 import com.theokanning.openai.completion.CompletionResult;
-import org.springframework.beans.factory.annotation.Value;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class OpenAiGround {
 
-    @Value("${openAi.apiKey}")
-    private String key;
+    private final AiService aiService;
 
     @PostMapping("/openAi")
     public CompletionResult ApiTest(@RequestBody String text) {
-        OpenAiService service = new OpenAiService(key);
-        CompletionRequest request = CompletionRequest.builder()
-            .model("text-davinci-002")
-            .temperature(0.7)
-            .prompt(text)
-            .maxTokens(256)
-            .topP(1.0)
-            .frequencyPenalty(0.0)
-            .presencePenalty(0.0)
-            .build();
-        return service.createCompletion(request);
+        return aiService.getAiTalk(text);
     }
 
 }
