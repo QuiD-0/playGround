854f139e98254a632ead1c1d9d6f26331cd0a4ba
QuiD-0 <wodnd101@naver.com>
2023-01-02 17:29:18 +0900

    changer 테스트

---
+++ src/test/java/com/quid/changer/Sample.java
@@ -0,0 +1,12 @@
+package com.quid.changer;
+
+public class Sample {
+
+    private int age;
+    private String name;
+
+    public Sample(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+}
