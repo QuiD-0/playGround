f2a476d486d5f7b8ba09da53421cc546241e9b86
QuiD-0 <wodnd101@naver.com>
2023-02-15 13:36:41 +0900

    singleton을 깨뜨리는 방법

--- src/main/java/com/quid/designPattern/singleton/App.java
+++ src/main/java/com/quid/designPattern/singleton/App.java
@@ -1,15 +1,31 @@
 package com.quid.designPattern.singleton;
 
+import java.lang.reflect.Constructor;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class App {
 
     @Test
-    public void test() {
+    public void makeSingleton() {
         Settings settings = Settings.getInstance();
         Settings settings2 = Settings.getInstance();
         Assertions.assertEquals(settings, settings2);
     }
 
+    @Test
+    public void breakSingletonUsingReflection() throws Exception {
+        Settings settings = Settings.getInstance();
+        Constructor<Settings> constructor = Settings.class.getDeclaredConstructor();
+        constructor.setAccessible(true);
+        Settings settings2 = constructor.newInstance();
+        Assertions.assertNotEquals(settings, settings2);
+    }
+
+    @Test
+    public void javaSingleton() {
+        Runtime runtime = Runtime.getRuntime();
+        System.out.println(runtime.maxMemory());
+        System.out.println(runtime.freeMemory());
+    }
 }
