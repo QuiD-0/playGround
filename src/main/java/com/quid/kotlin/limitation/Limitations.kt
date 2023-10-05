563569b9e15764be23a0c40c4565cef2146703c2
QuiD-0 <wodnd101@naver.com>
2023-10-05 10:09:14 +0900

    requireNotNull 스마트 캐스팅

--- src/main/java/com/quid/kotlin/limitation/Limitations.kt
+++ src/main/java/com/quid/kotlin/limitation/Limitations.kt
@@ -5,14 +5,23 @@ fun main() {
     val stack = CustomStack(listOf(1, 2, 3, 4, 5))
 
     stack.pop(1).also { println(it) }
+    val num: Int? = stack.peek()
+    requireNotNull(num) { "num is null" }
+    // smart cast
+    num.also { println(it) }
 }
 
 class CustomStack<T>(
     private var list: List<T> = mutableListOf()
 ){
+    private val isEmpty : Boolean
+        get() = list.isEmpty()
+
+    fun peek() : T? = list.firstOrNull()
+
     fun pop(num : Int) : List<T> {
-        require(list.isNotEmpty())
-        check(list.isNotEmpty())
+        require(num > 0) { "num must be positive" }
+        check(!isEmpty) { "list is empty" }
         val ret = list.take(num)
         list = list.drop(num)
         return ret
