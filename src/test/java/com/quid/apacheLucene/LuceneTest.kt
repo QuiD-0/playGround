e88f123c0dcd1cb43c9d643469fca719c6e57059
quid-0 <wodnd101@naver.com>
2024-03-30 16:41:23 +0900

    불필요한 의존성 제거

--- src/test/java/com/quid/apacheLucene/LuceneTest.kt
+++ src/test/java/com/quid/apacheLucene/LuceneTest.kt
@@ -3,24 +3,23 @@ package com.quid.apacheLucene
 import org.junit.jupiter.api.Test
 import org.slf4j.LoggerFactory
 
-class LuceneTest{
-    private val logger = LoggerFactory.getLogger(this::class.java)
+class LuceneTest {
+    private val log = LoggerFactory.getLogger(this::class.java)
     private val path = "src/test/resources/index"
     private val lucene = Lucene(path)
 
     @Test
-    fun indexingWithLucene(){
-        val personMaker = PersonMaker(10)
-        repeat(10){
+    fun indexingWithLucene() {
+        val personMaker = PersonMaker(1_000)
+        repeat(100) {
             personMaker.make()
                 .also { lucene.indexing(it) }
-                .also { logger.info("Indexing completed") }
         }
     }
 
     @Test
-    fun searchWithLucene(){
-        lucene.search("Person 3", 10000)
-            .also { println(it) }
+    fun searchWithLucene() {
+        lucene.search("Person 3", 10)
+            .also { log.info("$it") }
     }
 }
