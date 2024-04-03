6df5eaf4d58a5955ae6427b4e9fd6300a6dcf8db
QuiD-0 <wodnd101@naver.com>
2024-04-03 17:44:20 +0900

    테스트 수정

--- src/main/java/com/quid/spring3/restClient/NaverClient.java
+++ src/main/java/com/quid/spring3/restClient/NaverClient.java
@@ -1,5 +1,6 @@
 package com.quid.spring3.restClient;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestClient;
 
 public class NaverClient {
@@ -10,11 +11,14 @@ public class NaverClient {
         this.restClient = restClient;
     }
 
-    public String getNaver() {
+    public ResponseEntity<String> getNaver() {
         return restClient.get()
             .retrieve()
-            .toEntity(String.class)
-            .getBody();
+            .onStatus(it -> it.is4xxClientError() || it.is5xxServerError(),
+                ((request, response) -> {
+                    throw new RuntimeException("Error");
+                }))
+            .toEntity(String.class);
     }
 
 }
--- src/test/java/com/quid/spring3/restClient/NaverClientTest.java
+++ src/test/java/com/quid/spring3/restClient/NaverClientTest.java
@@ -1,8 +1,11 @@
 package com.quid.spring3.restClient;
 
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 @Disabled
 class NaverClientTest {
@@ -11,8 +14,8 @@ class NaverClientTest {
     @Test
     void getNaver() {
         NaverClient naverClient = new NaverClient(config.restClient());
-        String test = naverClient.getNaver();
-        System.out.println(test);
+        ResponseEntity<String> response = naverClient.getNaver();
+        Assertions.assertEquals(HttpStatus.OK, response.getStatusCode());
     }
 
 }
