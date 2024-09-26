1e87b9085979fc505915a45bf5fb4f91c76cfd81
quid-0 <wodnd101@naver.com>
2024-09-26 17:30:16 +0900

    kafka streams config 작성

--- src/main/java/com/quid/kafkaStreams/KafkaStreamsConfig.kt
+++ src/main/java/com/quid/kafkaStreams/KafkaStreamsConfig.kt
@@ -1,9 +1,17 @@
 package com.quid.kafkaStreams
 
+import org.apache.kafka.common.serialization.Serdes
+import org.apache.kafka.streams.StreamsConfig.APPLICATION_ID_CONFIG
+import org.apache.kafka.streams.StreamsConfig.BOOTSTRAP_SERVERS_CONFIG
+import org.apache.kafka.streams.StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG
+import org.apache.kafka.streams.StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.kafka.annotation.EnableKafka
 import org.springframework.kafka.annotation.EnableKafkaStreams
+import org.springframework.kafka.annotation.KafkaStreamsDefaultConfiguration
+import org.springframework.kafka.config.KafkaStreamsConfiguration
+
 
 @EnableKafka
 @EnableKafkaStreams
@@ -12,9 +20,17 @@ class KafkaStreamsConfig(
     private val kafkaHostProperties: KafkaHostProperties
 ) {
 
-    @Bean
-    fun getBootstrapServers(): String {
-        return kafkaHostProperties.bootstrapServers
+    @Bean(name = [KafkaStreamsDefaultConfiguration.DEFAULT_STREAMS_CONFIG_BEAN_NAME])
+    fun kStreamsConfig(): KafkaStreamsConfiguration {
+        return KafkaStreamsConfiguration(
+            mapOf(
+                APPLICATION_ID_CONFIG to "streams-app",
+                BOOTSTRAP_SERVERS_CONFIG to kafkaHostProperties.bootstrapServers,
+                DEFAULT_KEY_SERDE_CLASS_CONFIG to Serdes.String().javaClass.name,
+                DEFAULT_VALUE_SERDE_CLASS_CONFIG to Serdes.String().javaClass.name
+            )
+        )
     }
+
 }
 
