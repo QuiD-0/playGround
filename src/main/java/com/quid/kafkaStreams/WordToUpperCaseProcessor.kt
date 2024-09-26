2ba1ba647ef87ebe3fcb40d552ecc7376a778ea3
quid-0 <wodnd101@naver.com>
2024-09-26 20:10:54 +0900

    log 수정

--- src/main/java/com/quid/kafkaStreams/WordToUpperCaseProcessor.kt
+++ src/main/java/com/quid/kafkaStreams/WordToUpperCaseProcessor.kt
@@ -5,6 +5,7 @@ import org.apache.kafka.common.serialization.Serdes
 import org.apache.kafka.streams.StreamsBuilder
 import org.apache.kafka.streams.kstream.Consumed
 import org.apache.kafka.streams.kstream.ValueMapper
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 import java.util.*
@@ -12,14 +13,15 @@ import java.util.*
 
 @Component
 class WordToUpperCaseProcessor {
+    val log = LoggerFactory.getLogger(this::class.java)!!
 
     @Autowired
     fun buildPipeline(streamsBuilder: StreamsBuilder) {
         streamsBuilder
             .stream("input-topic", Consumed.with(STRING_SERDE, STRING_SERDE))
-            .peek() { key, value -> println("INPUT :: key: $key, value: $value") }
+            .peek { key, value -> log.info("INPUT :: key: $key, value: $value") }
             .mapValues(ValueMapper { obj: String -> obj.uppercase(Locale.getDefault()) } as ValueMapper<String, String>)
-            .peek { key, value -> println("OUTPUT :: key: $key, value: $value") }
+            .peek { key, value -> log.info("OUTPUT :: key: $key, value: $value") }
             .to("output-topic")
     }
 
