ae640ef95176ed85270c758a09e8ec93fe63cda3
quid-0 <wodnd101@naver.com>
2025-04-03 22:09:11 +0900

    retry

--- src/main/java/com/quid/resilience4j/DemoClient.kt
+++ src/main/java/com/quid/resilience4j/DemoClient.kt
@@ -3,7 +3,6 @@ package com.quid.resilience4j
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker
 import io.github.resilience4j.retry.annotation.Retry
 import org.slf4j.LoggerFactory
-import org.springframework.http.HttpStatusCode
 import org.springframework.stereotype.Component
 import org.springframework.web.client.RestClient
 
@@ -13,20 +12,21 @@ class DemoClient(
 ) {
     private val log = LoggerFactory.getLogger(this::class.java)!!
 
-    @Retry(name = "demoClient", fallbackMethod = "fallback")
+    @Retry(name = "demoClient", fallbackMethod = "retryFallback")
     @CircuitBreaker(name = "demoClient", fallbackMethod = "fallback")
     fun getData(): String {
         return client.get()
             .retrieve()
-            .onStatus(
-                { status: HttpStatusCode -> status.is4xxClientError },
-                { _, _ -> throw RuntimeException("Client error") }
-            )
             .toEntity(String::class.java)
             .body!!
     }
 
-    fun fallback(ex: Throwable):String {
+    fun retryFallback(ex: Throwable): String {
+        log.error(ex.message, ex)
+        return "Retry fallback response"
+    }
+
+    fun fallback(ex: Throwable): String {
         log.error(ex.message, ex)
         return "Fallback response"
     }
