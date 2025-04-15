f7bae614cb88b4a94cdb04de89e87aa32f3ddeb3
quid-0 <wodnd101@naver.com>
2025-04-15 11:00:42 +0900

    메소드 참조

--- src/main/java/com/quid/java/lambda/LambdaStaticMethod.java
+++ src/main/java/com/quid/java/lambda/LambdaStaticMethod.java
@@ -12,6 +12,10 @@ public class LambdaStaticMethod {
         return a + b;
     }
 
+    int testMethod2(int a, int b) {
+        return a + b;
+    }
+
     public void run() {
         // 메소드 참조
         // 메소드 참조는 메소드의 시그니처를 기반으로 메소드를 찾음 java/lang/Integer.sum(II)I,
@@ -19,10 +23,14 @@ public class LambdaStaticMethod {
 
         // 람다 표현식
         // 바이트코드를 보면 람다 표현식이 메소드 참조로 변환됨, 새 synthetic 정적 메서드 생성 후 호출됨
-        BiFunction<Integer, Integer, Integer> lambda = (a, b) -> a + b;
+        BiFunction<Integer, Integer, Integer> lambda = (a, b) -> a - b;
 
-        // 메소드 참조를 람다 표현식으로 변환
+        // 정적 메소드를 람다 표현식으로 변환
         // 새 정적 메서드 생성 X, com/quid/java/lambda/LambdaStaticMethod.testMethod(II)I 정의 되어 있는 메서드 호출됨
         BiFunction<Integer, Integer, Integer> lambdaFromMethodRef = LambdaStaticMethod::testMethod;
+
+        // 메소드 참조를 람다 표현식으로 사용
+        // 새 정적 메서드 생성 X, com/quid/java/lambda/LambdaStaticMethod.testMethod2(II)I 정의 되어 있는 메서드 호출됨
+        BiFunction<Integer, Integer, Integer> lambdaFromMethodRef2 = this::testMethod2;
     }
 }
